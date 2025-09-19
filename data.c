03603828d5c0027fc0c713561f5bf7e19c89cd82
marykaris721-sudo <marykaris721@gmail.com>
2025-09-19 13:10:03 +0300

    Add files via upload

---
+++ data.c
@@ -0,0 +1,26 @@
+//programme displaying height,phonenumber and weight
+/*
+Name: Mary Wanjiru
+Reg no.: CT100/G/25276/24
+Description: height, phone number,weight
+*/
+
+#include<stdio.h>
+int main()
+{
+	int weight;
+	printf ("enter weight");
+	scanf  ("%d", &weight);
+	printf ("your weight is %d\n",weight);
+	
+	int height;
+	printf ("enter height");
+	scanf ("%d", &height);
+	printf ("your height is %d\n",height);
+	
+	int phone;
+	printf ("enter phone number");
+	scanf ("%d", &phone);
+	printf ("your age is %d\n",phone);
+	
+}
---
+++ units.c
@@ -0,0 +1,19 @@
+//units semester 1
+/*
+Name: Mary Wanjiru
+Reg:CT100/G/25276/24
+Description: units in this semester
+ 1*/
+
+#include<stdio.h>
+int main (){
+	printf ("UNITS IN BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY");
+	printf ("SPI 2101 Introduction to Programming and Algorithms\n");
+	printf ("SPI 2102 Computer Systems and Organization\n");
+	printf("SPI 2103 Introduction to Computer Appliction\n");
+	printf ("SPI 2120 Essentials of Accounting\n");
+	printf ("SPM 2104 Mathematics for Science\n");
+	printf ("UCU 2105 Fundermentals of ICT\n");
+	printf ("UCU 2106 Communication and Information Literacy\n");
+	
+}
