0cd839c83c98cd16eb5356e269a03562adf3e514
marykaris721-sudo <marykaris721@gmail.com>
2025-10-09 11:17:25 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,14 @@
+/*
+Name:Mary Wanjiru Kariuki
+Reg: CT100/G/25276/24
+Description:a programme to print from 100 to 50in descending order
+
+*/
+#include <stdio.h>
+int main(){
+int i;
+	for (i=100; i>=50; i--) {
+		printf("%d\t",i);
+	}
+return 0;	
+}
